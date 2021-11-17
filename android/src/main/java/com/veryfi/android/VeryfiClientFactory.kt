c6e68df5a26cf380a56db9340ee35a8103497958
Sebastian Carmona <sebastiancarmona@hotmail.ca>
2021-11-17 18:59:17 -0500

    Set apiVersion as parameter and by default 8.

--- android/src/main/java/com/veryfi/android/VeryfiClientFactory.kt
+++ android/src/main/java/com/veryfi/android/VeryfiClientFactory.kt
@@ -10,14 +10,16 @@ object VeryfiClientFactory {
      * @param clientSecret the [String] provided by Veryfi.
      * @param username the [String] provided by Veryfi.
      * @param apiKey the [String] provided by Veryfi.
+     * @param apiVersion [Int] api version to use by default 8.
      * @return the new instance.
      */
     fun createClient(
         clientId: String,
         clientSecret: String,
         username: String,
-        apiKey: String
+        apiKey: String,
+        apiVersion: Int = 8
     ): Client {
-        return ClientImpl(ClientData(clientId, clientSecret, username, apiKey))
+        return ClientImpl(ClientData(clientId, clientSecret, username, apiKey, apiVersion))
     }
 }
